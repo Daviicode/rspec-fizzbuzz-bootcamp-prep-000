354b82ca0f4b22804d7b0aa9e4df8c7a4d5bdca1
David zhang <d.ydzdavid@yahoo.com>
2019-02-26 23:33:27 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(int)
-  if (int % 3 == 0 && int % 5 == 0)
-    "FizzizzBuzz"
-  elsif int % 3 == 0
+def fizzBuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    "FizzBuzz"
+  elsif number % 3 == 0
     "Fizz"
-  elsif int % 5 == 0
+  elsif number % 5 == 0
     "Buzz"
   else
-    nil
+    number
   end
 end
